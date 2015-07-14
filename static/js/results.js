d430f7010a52f4c1691afd3c0f5f4ed2f145b539
Pedro Ferreira <plferreira4@gmail.com>
2015-07-14 17:53:26 +0100

    - position the pie chart next to the results table - add the same styling off the poll table to the results table - add a legend to the pie chart

--- static/js/results.js
+++ static/js/results.js
@@ -16,15 +16,23 @@ var total = 0;
 
 for (var a = 0 ; a < votesElements.length ; a++)
     {
-    var voteCount = votesElements[ a ].getAttribute( 'data-votes' );
+    var element = votesElements[ a ];
+    var voteCount = element.getAttribute( 'data-votes' );
     var voteNumber = parseInt( voteCount );
+    var optionName = element.previousElementSibling.innerHTML;
 
     total += voteNumber;
 
-    votes.push( voteNumber );
+    votes.push({
+            name: optionName,
+            count: voteNumber
+        });
     }
 
-drawChart( votes, total );
+if ( total !== 0 )
+    {
+    drawChart( votes, total );
+    }
 };
 
 
@@ -33,6 +41,9 @@ function drawChart( votes, total_votes )
 var centerX = CANVAS.width / 2;
 var centerY = CANVAS.height / 2;
 var radius;
+var fontSize = 20;
+var legendX = CANVAS.width - 170;
+var legendY = fontSize;
 
 if ( centerX < centerY )
     {
@@ -44,8 +55,18 @@ else
     radius = centerY;
     }
 
+    // have the chart starting at the left of the canvas (to give room for the legend)
+centerX = radius;
+
+
 var ctx = CANVAS.getContext( '2d' );
 
+   // set the font of the legend
+ctx.font = fontSize + 'px monospace';
+
+    // set the length/color of the line separating the different parts of the pie chart
+ctx.lineWidth = 2;
+ctx.strokeStyle = 'white';
 
 var startAngle = 0;
 
@@ -54,6 +75,9 @@ var colorsPosition = 0;
 
 for (var a = 0 ; a < votes.length ; a++)
     {
+    var vote = votes[ a ];
+
+        // get one of the colours
     var color = colors[ colorsPosition ];
 
     colorsPosition++;
@@ -63,7 +87,8 @@ for (var a = 0 ; a < votes.length ; a++)
         colorsPosition = 0;
         }
 
-    var percentage = votes[ a ] / total_votes;
+        // draw in the pie chart
+    var percentage = vote.count / total_votes;
     var endAngle = startAngle + percentage * Math.PI * 2;
 
     ctx.beginPath();
@@ -73,11 +98,17 @@ for (var a = 0 ; a < votes.length ; a++)
     ctx.closePath();
     ctx.fill();
 
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = 'white';
+        // draw the separating line
     ctx.stroke();
 
+        // add to the legend
+    ctx.fillText( vote.name, legendX, legendY );
+
+        // advance the starting angle
     startAngle = endAngle;
+
+        // draw the next legend below the current one
+    legendY += fontSize;
     }
 }
 
